6bcfba72b67f015cfe031c5e9e2f2143536d1272
zudilinv <102663010+zudilinv@users.noreply.github.com>
2023-10-16 14:43:41 +0400

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
+import { HW4 } from './HW/4/HW4';
 
+//test commit
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<HW1 />*/}
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
     </div>
   );
 }
