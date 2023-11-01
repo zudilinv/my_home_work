e530d072f001204d5601523a282a35dbfe19fde1
zudilinv <102663010+zudilinv@users.noreply.github.com>
2023-11-01 13:33:22 +0400

    Homework_4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,11 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: ()=> void
+  name: string
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack()
   };
 
   return (
