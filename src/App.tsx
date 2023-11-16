a897c13d7f15f8b91a87cab3e18b59c148edc717
zudilinv <102663010+zudilinv@users.noreply.github.com>
2023-11-17 00:45:19 +0400

    Homework_2_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import s from './App.module.css'
-import HW1 from './s2-homeworks/hw01/HW1'
+import {HW1} from "./HW2/hw01/HW1"
+import {HW2} from "./HW2/hw02/HW2";
+
+
 
 function App() {
     return (
         <div className={s.App}>
             <HW1/>
-            {/*<HW2/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
 
