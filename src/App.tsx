091ae59ac25a62dde61adfcb190c5c28fee00fa7
zudilinv <102663010+zudilinv@users.noreply.github.com>
2023-11-25 20:32:00 +0400

    Homework_2_3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import {HW1} from "./HW2/hw01/HW1"
 import {HW2} from "./HW2/hw02/HW2";
+import HW3 from "./HW2/hw03/HW3";
 
 
 
@@ -10,7 +11,7 @@ function App() {
         <div className={s.App}>
             <HW1/>
             <HW2/>
-            {/*<HW3/>*/}
+            <HW3/>
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
